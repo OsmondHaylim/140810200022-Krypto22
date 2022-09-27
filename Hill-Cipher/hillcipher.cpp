25687d846b1b3e4835796d33f4547c090444601b
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2022-09-27 23:52:47 +0700

    2

--- Hill-Cipher/hillcipher.cpp
+++ Hill-Cipher/hillcipher.cpp
@@ -18,7 +18,46 @@ void keyOutput(int key[][2]){
         cout << endl;
     }
 }
-
+int** inverse(int key[][2]){
+    int inv[2][2];int invKey[2][2];
+    inv[0][0]=(int)(key[1][1]);
+	inv[0][1]=(int)((-1) * key[0][1]);
+	inv[1][0]=(int)((-1) * key[1][0]);
+	inv[1][1]=(int)(key[0][0]);
+    int det = (key[0][0]*key[1][1]) - (key[0][1]*key[1][0]);
+    int detInv = 0;
+    int flag = 0;
+    
+    for (int i = 0; i < 26; i++){
+        flag = (det * i) % 26;
+        if (flag < 0){
+        	flag = flag + 26;
+		}
+        if (flag == 1){
+            detInv = i;
+        }
+    }
+	for(int i=0 ; i<2 ; i++)
+		for(int j=0 ; j<2 ; j++){
+			if ( inv[i][j] < 0 ) {
+				int tempNumber = inv[i][j]*detInv;
+			    invKey[i][j] = ((tempNumber%26) + 26)%26;
+			}
+			else{
+				invKey[i][j] = (inv[i][j]*detInv%26);
+			}
+		}
+    
+    int** array2D = 0;
+      array2D = new int*[2];
+      for (int h = 0; h < 2; h++){
+            array2D[h] = new int[2];
+            for (int w = 0; w < 2; w++){
+                  array2D[h][w] = invKey[h][w];
+            }
+      }   
+    return array2D;
+}
 
 string Encrypt(int key[][2], string text){
     // ascii dec range 65 ... 90 (ascii for A to Z)
@@ -55,36 +94,8 @@ string Decrypt(int key[][2], string text){
     string decrypted;
     int con[2];
     int con2 = 0;
-    int inv[2][2];int invKey[2][2];
-    inv[0][0]=(int)(key[1][1]);
-	inv[0][1]=(int)((-1) * key[0][1]);
-	inv[1][0]=(int)((-1) * key[1][0]);
-	inv[1][1]=(int)(key[0][0]);
-    int det = (key[0][0]*key[1][1]) - (key[0][1]*key[1][0]);
-    int detInv = 0;
-    int flag = 0;
     
-    for (int i = 0; i < 26; i++){
-        flag = (det * i) % 26;
-        if (flag < 0){
-        	flag = flag + 26;
-		}
-        if (flag == 1){
-            detInv = i;
-        }
-    }
-	for(int i=0 ; i<2 ; i++)
-		for(int j=0 ; j<2 ; j++){
-			if ( inv[i][j] < 0 ) {
-				int tempNumber = inv[i][j]*detInv;
-			    invKey[i][j] = ((tempNumber%26) + 26)%26;
-			}
-			else{
-				invKey[i][j] = (inv[i][j]*detInv%26);
-			}
-		}
-
-
+    int** invKey = inverse(key);
     int z = 0;
     if(text.length()%2 == 0){
         z = text.length()/2;
@@ -115,7 +126,7 @@ void elCheck(int key[][2], string text){
     int det = 0;
     det += (key[0][0]*key[1][1]);
     det -= (key[0][1]*key[1][0]);
-    int result = min(det, 26);
+    int result = min(det%26, 26);
     while (result > 0) {
         if (det % result == 0 && 26 % result == 0) {
             break;
@@ -127,40 +138,79 @@ void elCheck(int key[][2], string text){
         cout << "Matrix is not inversible mod 26\n";
     }else{
         cout << "Can be Decrypted\n";
-        cout << "Hasil Enkripsi : " << endl << Decrypt(key, Encrypt(key, text)) << endl;
-    }
-}
+        cout << "Hasil Enkripsi : " << endl << Decrypt(key, text) << endl;
+    }}
+// void keySearch(int key[][2], string text, string text2){
+//     int con[2][2];
+//     int con2[2][2];
+//     int z = 0;
+//     if(text.length()%2 == 0){
+//         z = text.length()/2;
+//     }else{
+//         z = (text.length()/2) + 1;
+//     }
+//     for(int i = 0; i < 2; i++){
+//         con[0][0] = 23;con[1][0] = 23;con[0][1] = 23;con[1][1] = 23;
+//         for(int j = 0; j < 2; j++){ // alphabet -> ASCII number
+//             if((int)text[j + (i*2)] >= 97 && (int)text[j + (i*2)] <= 122) {  // alfabet kecil
+//                 con[j][i] = (int)text[j + (i*2)] - 97;
+//             }else if((int)text[j + (i*2)] >= 65 && (int)text[j + (i*2)] <= 90) {  // alfabet kapital
+//                 con[j][i] = (int)text[j + (i*2)] - 65;
+//             }else if(isblank(text[j + (i*2)])){
+//                 con[j][i] = 23;
+//             }}
+//     }
+//     for(int i = 0; i < 2; i++){
+//         con2[0][0] = 23;con2[1][0] = 23;con2[0][1] = 23;con2[1][1] = 23;
+//         for(int j = 0; j < 2; j++){ // alphabet -> ASCII number
+//             if((int)text2[j + (i*2)] >= 97 && (int)text2[j + (i*2)] <= 122) {  // alfabet kecil
+//                 con2[j][i] = (int)text2[j + (i*2)] - 97;
+//             }else if((int)text2[j + (i*2)] >= 65 && (int)text2[j + (i*2)] <= 90) {  // alfabet kapital
+//                 con2[j][i] = (int)text2[j + (i*2)] - 65;
+//             }else if(isblank(text2[j + (i*2)])){
+//                 con2[j][i] = 23;
+//             }}
+//     }
+// }
 int main(){
     string text;
     int resp = 0;
     int key[2][2];
 
     cout << "\nMenu : ";
-    cout << "\n1. Enkripsi text : ";
-    cout << "\n2. Dekripsi text : ";
-    cout << "\n3. Cari Key : ";
+    cout << "\n1. Enkripsi text ";
+    cout << "\n2. Dekripsi text ";
+    cout << "\n3. Cari Key ";
+    cin >> resp;
     switch(resp) {
         case 1:
             cout << "\nMasukkan text : ";
-            getline(cin, text);
+            cin >> text;
 
             cout << "\nMasukkan kunci : \n";
             keyInput(key);
 
             cout << "Hasil Enkripsi : " << endl << Encrypt(key, text) << endl;
-        break;
+            break;
         case 2:
             cout << "\nMasukkan text : ";
-            getline(cin, text);
+            cin >> text;
 
             cout << "\nMasukkan kunci : \n";
             keyInput(key);
 
-            cout << "Hasil Dekripsi : " << endl << Decrypt(key, text) << endl;
-        break;
+            elCheck(key, text);
+            break;
         case 3:
-    // code block
-        break;
+            // string text2;
+            // cout << "\nMasukkan text sebelum enkripsi: ";
+            // cin >> text;
+
+            // cout << "\nMasukkan text setelah enkripsi: ";
+            // cin >> text;
+
+            // keySearch(key, text, text2);
+            break;
         default:
             cout << "Respon tidak sesuai";
 }
